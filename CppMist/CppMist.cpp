bbf52de230b310835c5c2a479602a5f9efc921df
SmallNightLight <matthiaskruis@gmail.com>
2024-09-04 15:48:28 +0200

    Visual Studio project Init
    Added the CPP project for visual studio

---
+++ CppMist/CppMist.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Source/Headers/ComponentManager.h"
+
+int main()
+{
+
+    std::cout << "Initializing ECS\n";
+}
---
+++ CppMist/Source/Headers/Settings.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <bitset>
+
+using byte = std::uint8_t;
+
+using Entity = std::uint32_t;
+using ComponentType = std::uint8_t;
+
+const Entity MAXENTITIES = 2000;
+const ComponentType MAXCOMPONENTS = 32;
+
+using Signature = std::bitset<MAXCOMPONENTS>;
